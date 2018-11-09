85bde93fd27ef002b98e08c65446cde79e18c31a
KewlKris <chambca@gmail.com>
2018-11-08 18:36:53 -0600

    Fixed the flickering issue.

--- src/snake/SnakeCanvas.java
+++ src/snake/SnakeCanvas.java
@@ -6,13 +6,13 @@ import java.awt.event.*;
 public class SnakeCanvas extends Canvas
 {
     Image frameBuffer;
+    Graphics buf;
     public SnakeCanvas()
     {
         this.setSize(SnakeSettings.WINDOW_SIZE);
         this.setBackground(new Color(210, 210, 210));
         
         this.addKeyListener(new SnakeListener());
-        //frameBuffer = new Image();
     }
     
     private class SnakeListener implements KeyListener
@@ -28,15 +28,31 @@ public class SnakeCanvas extends Canvas
     
     public void paint(Graphics g)
     {
-        
-        SnakeGame.view.drawBorder(g);
-        SnakeGame.view.fillBackground(g);
-        SnakeGame.view.drawGrid(g);
+        if (frameBuffer == null)
+        {
+            try
+            {
+                frameBuffer = createImage(this.getWidth(), this.getHeight());
+                buf = frameBuffer.getGraphics();
+            } catch (NullPointerException e)
+            {
+                return;
+            }
+        }
+        SnakeGame.view.drawBorder(buf);
+        SnakeGame.view.fillBackground(buf);
+        SnakeGame.view.drawGrid(buf);
         
         if (SnakeGame.gameInProgress)
         {
-            SnakeGame.drawEntities(g);
+            SnakeGame.drawEntities(buf);
         }
+        g.drawImage(frameBuffer, 0, 0, this);
         repaint();
+    }
+    
+    public void update(Graphics g)
+    {
+        paint(g);
     }
 }
