1845ab24e44b0a362f8401ecf35a1f4982607aaf
chamb <chamb@Kewl-PC>
2018-11-08 21:04:42 -0600

    A test

--- src/snake/SnakeTimer.java
+++ src/snake/SnakeTimer.java
@@ -5,7 +5,7 @@ import java.time.Instant;
 
 public class SnakeTimer extends SwingWorker<Void, Void>
 {
-    public static final float TICKRATE = 0.1f; //Time in seconds
+    public static final float TICKRATE = 0.05f; //Time in seconds
     
     public Void doInBackground()
     {
@@ -14,7 +14,7 @@ public class SnakeTimer extends SwingWorker<Void, Void>
             Instant start = Instant.now();
             while(((float)(Instant.now().toEpochMilli() - start.toEpochMilli()) / 1000f) < TICKRATE)
             {
-                
+                //Delay
             }
             SnakeGame.tick();
         }
